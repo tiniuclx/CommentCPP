34dc1736d7b2a49794a8178e7d66bc30c14640b1
tiniuclx <tiniuc.alexandru@gmail.com>
2017-03-13 19:26:00 +0000

    Added comment generation stubs.

--- commentcpp.py
+++ commentcpp.py
@@ -6,16 +6,31 @@ Created on 11 Mar 2017
 import argparse
 import re
 import tracery
+
+
+def gen_sl_comment():
+    """Procedurally generates a single-line comment, different for every call.
+    """
+    # TODO: Implement this
+    return r'// Placeholder single-line comment'
+
+
+def gen_ml_comment():
+    """Procedurally generates a multi-line comment, different for every call."""
+    # TODO: Implement this
+    return r'/* Placeholder multi \n line comment */'
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='File to add comments to')
-    args = parser.parse_args()    
+    args = parser.parse_args()
     file = open(args.path)
     contents = file.read()
     file.close()
 else:
     contents = r'//Placeholder'
-    
+
 # Single-line comment regular expression
 sl_regex = re.compile(r'//(.*)')
 # List of single line comment matches
@@ -25,12 +40,16 @@ ml_regex = re.compile(r'/\*(.*?)\*/', re.RegexFlag.DOTALL)
 # List of multi line comment matches
 ml_coms = ml_regex.finditer(contents)
 
+# idea: to keep indentation consistent between code and inserted comment:
+# make regex that searches for start of line, followed by 0 or more whitespace
+# followed by non-whitespace character
+
 # for line in sl_coms:
 #     print(line.group())
 # for comment in ml_coms:
 #     print(comment.group(1))
 # group() returns the whole match, group(1) returns only the comments
 
-output = ml_regex.sub(r'/* Placeholder \n   multiline \n   comment \n */', contents)
-output = sl_regex.sub(r'//placeholder single-line comment', output)
+output = ml_regex.sub(gen_ml_comment(), contents)
+output = sl_regex.sub(gen_sl_comment(), output)
 print(output)
