7984f30a004421202ec2b64c8b0fdd61fd89f80e
tiniuclx <tiniuc.alexandru@gmail.com>
2017-03-24 12:34:51 +0000

    removed main-checking: unnecessary. added parser idea. shouldn't be that hard to make a c parser!

--- commentcpp.py
+++ commentcpp.py
@@ -88,17 +88,16 @@ def gen_ml_comment(match):
     return '/* Placeholder multi \n   line comment \n */'
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='file to add comments to')
-    parser.add_argument('-ow','--overwrite',action='store_true', help='overwrite the input file')
-    parser.add_argument('-d','--destination', help='destination of output file')
-    args = parser.parse_args()
-    file = open(args.path)
-    contents = file.read()
-    file.close()
-else:
-    contents = r'//Placeholder'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='file to add comments to')
+parser.add_argument('-ow','--overwrite',action='store_true', help='overwrite the input file')
+parser.add_argument('-d','--destination', help='destination of output file')
+args = parser.parse_args()
+file = open(args.path)
+contents = file.read()
+file.close()
+
 
 # Single-line comment regular expression
 sl_regex = re.compile(r'//(.*)')
@@ -113,6 +112,12 @@ ml_coms = ml_regex.finditer(contents)
 # idea: to keep indentation consistent between code and inserted comment:
 # make regex that searches for start of line, followed by 0 or more whitespace
 # followed by non-whitespace character
+# can also be done by keeping track of line number and line location
+
+# other idea: write a c++ parser (or, to reduce the scope, a c parser) as outlined in
+# CraftingInterpreters.com
+# using this parser, add contextually-sensitive comments (i.e. referring to specific
+# variables in the source file) to make everything extra-confusing!
 
 output = ml_regex.sub(gen_ml_comment, contents)
 output = sl_regex.sub(gen_sl_comment, output)
